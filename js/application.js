1e412ac61c3baa95785b7c43259b0a9aa6ed57e4
Jonas Mueller <jonas@muellers.one>
2019-02-21 15:42:15 +0100

    Show varying quotes

--- js/application.js
+++ js/application.js
@@ -3,6 +3,23 @@ function showproject(projectid) {
   $("#project-description " + projectid).fadeIn("slow");
 }
 
+function showquote(quoteid) {
+  $("#quotes .media").hide();
+  $("#quotes " + quoteid).css("display", "flex").hide().fadeIn("slow");
+}
+
+var quotes = ["#quote-1", "#quote-2", "#quote-3"];
+function cycle_quotes(n = 0) {
+  showquote(quotes[n]);
+  if (n >= quotes.length - 1) {
+    console.log("Showing quote " + quotes[0]);
+    setTimeout(function() { cycle_quotes(0); }, 5000);
+  } else {
+    console.log("Showing quote " + quotes[n+1]);
+    setTimeout(function() { cycle_quotes(n+1); }, 5000);
+  }
+}
+
 $(document).ready(function() {
 
   // Navbar burger
@@ -18,6 +35,7 @@ $(document).ready(function() {
 
   // Project descriptions
   showproject("#soulfood-delight");
+  cycle_quotes();
 
   $("#projectnav a").click(function() {
     showproject($(this).data("target"));
